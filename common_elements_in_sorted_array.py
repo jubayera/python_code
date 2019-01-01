9605b9bed2a55f57145cb808e121af91525c7718
jubayera <cajubuvic@gmail.com>
2018-12-31 20:08:13 -0800

    Create common_elements_in_sorted_array.py

---
+++ common_elements_in_sorted_array.py
@@ -0,0 +1,32 @@
+# find the common elements in two sorted array
+def common_elements(list1, list2):
+    p1 = 0
+    p2 = 0
+    result = []
+
+    while p1 < len(list1) and p2 < len(list2):
+        if list1[p1] == list2[p2]:
+            result.append(list1[p1])
+            p1 += 1
+            p2 += 1
+        elif list1[p1] > list2[p2]:
+            p2 += 1
+        else:
+            p1 += 1
+    
+    return result
+
+list_a1 = [1, 3, 4, 6, 7, 9]
+list_a2 = [1, 2, 4, 5, 9, 10]
+
+print common_elements(list_a1, list_a2)
+
+list_b1 = [1, 2, 9, 10, 11, 12]
+list_b2 = [0, 1, 2, 3, 4, 5, 8, 9, 10, 12, 14, 15]
+
+print common_elements(list_b1, list_b2)
+
+list_c1 = [0, 1, 2, 3, 4, 5]
+list_c2 = [6, 7, 8, 9, 10, 11]
+
+print common_elements(list_c1, list_c2)
